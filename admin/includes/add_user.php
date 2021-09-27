2ad6ce22eda2db1e64c84ff2b74e6e690a0fdea5
kushtrimsalihu <kushttrim.salihu@gmail.com>
2021-09-27 02:15:15 +0200

    Updated register and password crypt

--- admin/includes/add_user.php
+++ admin/includes/add_user.php
@@ -1,37 +1,40 @@
 <?php
-if(isset($_POST['create_user'])){
+if (isset($_POST['create_user'])) {
 
     if ($_POST['create_user']) {
-     $user_firstname = $_POST['user_firstname'];
-    $user_lastname = $_POST['user_lastname'];
-    $user_role = $_POST['user_role'];
+        $user_firstname = $_POST['user_firstname'];
+        $user_lastname = $_POST['user_lastname'];
+        $user_role = $_POST['user_role'];
 
-     $user_image = $_FILES['image']['name'];
-     $user_image_temp = $_FILES['image']['tmp_name'];
+        $user_image = $_FILES['image']['name'];
+        $user_image_temp = $_FILES['image']['tmp_name'];
 
-    $username = $_POST['username'];
-    $user_email = $_POST['user_email'];
+        $username = $_POST['username'];
+        $user_email = $_POST['user_email'];
 
-    // $options = [12];
-    $user_password = $_POST['user_password'];
-    //$user_password=password_hash($user_password,PASSWORD_BCRYPT);
+        $user_password = $_POST['user_password'];
+        $user_password = password_hash($user_password, PASSWORD_BCRYPT);
 
-    move_uploaded_file($user_image_temp, "../user_images/$user_image");
+        move_uploaded_file($user_image_temp, "../user_images/$user_image");
 
-    $query = "INSERT INTO users (user_image,user_firstname, user_lastname, user_role, username, user_email, user_password) 
+        if (!empty($user_firstname) && !empty($user_lastname) && !empty($user_role) && !empty($user_image)
+            && !empty($username) && !empty($user_email) && !empty($user_password)) {
+
+            $query = "INSERT INTO users (user_image,user_firstname, user_lastname, user_role, username, user_email, user_password)
     VALUES ('{$user_image}','{$user_firstname}','{$user_lastname}','{$user_role}','{$username}','{$user_email}','{$user_password}')";
 
+            $create_user_query = mysqli_query($connection, $query);
 
-    $create_user_query = mysqli_query($connection,$query);
+            confirm($create_user_query);
 
+            $message = "<div class='alert alert-success'>User Created: <a href='users.php'>View Users</a></div> ";
 
-  confirm($create_user_query);
-
-  echo "<div class='alert alert-success'>User Created: <a href='users.php'>View Users</a></div> ";
-
+        } else {
+            $message = "<div class='alert alert-warning'>All fields are required.</div> ";
+        }
     }
-   
-
+} else {
+    $message = "";
 }
 
 ?>
@@ -40,7 +43,7 @@ if(isset($_POST['create_user'])){
 
 
 <form action="" method="post" enctype="multipart/form-data">
-
+ <?php echo $message; ?>
     <div class="form-group">
         <label for="post_image">Image</label>
         <input type="file" name="image">
--- admin/profile.php
+++ admin/profile.php
@@ -10,16 +10,16 @@ $query = "SELECT * FROM users WHERE username = '{$username}'";
 
 $select_user_profile_query = mysqli_query($connection,$query);
 
-while ($row = mysqli_fetch_array($select_user_profile_query)) {
-    $user_id = $row['user_id'];
-    $user_image = $row['user_image'];
-    $username = $row['username'];
-    $user_password = $row['user_password'];
-    $user_firstname = $row['user_firstname'];
-    $user_lastname = $row['user_lastname'];
-    $user_email = $row['user_email'];
-    $user_role = $row['user_role'];
-}
+                while ($row = mysqli_fetch_array($select_user_profile_query)) {
+                    $user_id = $row['user_id'];
+                    $user_image = $row['user_image'];
+                    $username = $row['username'];
+                    $user_password = $row['user_password'];
+                    $user_firstname = $row['user_firstname'];
+                    $user_lastname = $row['user_lastname'];
+                    $user_email = $row['user_email'];
+                    $user_role = $row['user_role'];
+                }
 
 }
 ?>
@@ -36,7 +36,7 @@ if (isset($_POST['edit_user'])) {
 
     $username = $_POST['username'];
     $user_email = $_POST['user_email'];
-    // $options = [12];
+
     $user_password = $_POST['user_password'];
     //$user_password=password_hash($user_password,PASSWORD_BCRYPT);
 
@@ -92,33 +92,35 @@ if (isset($_POST['edit_user'])) {
 
                     <!-- Profile Form -->
 
-                    
-<form action="" method="post" enctype="multipart/form-data">
 
-<div class="form-group">
-    <label for="post_image">Image</label>
-    <img width="100" src="../user_images/<?php echo $user_image; ?>" alt="">
-    <input type="file" value="<?php echo ''; ?>" name="image">
-</div>
+                    <form action="" method="post" enctype="multipart/form-data">
 
-<div class="form-group">
-    <label for="firstname">Firstname</label>
-    <input type="text" value="<?php echo $user_firstname; ?>" class="form-control" name="user_firstname">
-</div>
+                        <div class="form-group">
+                            <label for="post_image">Image</label>
+                            <img width="100" src="../user_images/<?php echo $user_image; ?>" alt="">
+                            <input type="file" value="<?php echo ''; ?>" name="image">
+                        </div>
 
-<div class="form-group">
-    <label for="lastname">Lastname</label>
-    <input type="text" value="<?php echo $user_lastname; ?>" class="form-control" name="user_lastname">
-</div>
+                        <div class="form-group">
+                            <label for="firstname">Firstname</label>
+                            <input type="text" value="<?php echo $user_firstname; ?>" class="form-control"
+                                name="user_firstname">
+                        </div>
+
+                        <div class="form-group">
+                            <label for="lastname">Lastname</label>
+                            <input type="text" value="<?php echo $user_lastname; ?>" class="form-control"
+                                name="user_lastname">
+                        </div>
 
 
 
 
-<div class="form-group">
+                        <div class="form-group">
 
-    <select class="form-select form-select-lg" name="user_role" id="user_role">
-        <option value="subscriber"><?php echo $user_role; ?></option>
-        <?php
+                            <select class="form-select form-select-lg" name="user_role" id="user_role">
+                                <option value="subscriber"><?php echo $user_role; ?></option>
+                                <?php
 if ($user_role == 'admin') {
 echo "<option value='subscriber'>subscriber</option>";
 } else {
@@ -126,31 +128,33 @@ echo "<option value='admin'>admin</option>";
 }
 ?>
 
-    </select>
-</div>
+                            </select>
+                        </div>
 
 
 
 
-<div class="form-group">
-    <label for="Username">Username</label>
-    <input type="text" value="<?php echo $username ?>" class="form-control" name="username">
-</div>
+                        <div class="form-group">
+                            <label for="Username">Username</label>
+                            <input type="text" value="<?php echo $username ?>" class="form-control" name="username">
+                        </div>
 
-<div class="form-group">
-    <label for="Email">Email</label>
-    <input type="email" value="<?php echo $user_email; ?>" class="form-control" name="user_email" id="">
-</div>
+                        <div class="form-group">
+                            <label for="Email">Email</label>
+                            <input type="email" value="<?php echo $user_email; ?>" class="form-control"
+                                name="user_email" id="">
+                        </div>
 
-<div class="form-group">
-    <label for="Password">Password</label>
-    <input type="password" value="<?php echo $user_password; ?>" class="form-control" name="user_password" id="">
-</div>
+                        <div class="form-group">
+                            <label for="Password">Password</label>
+                            <input type="password" value="<?php echo $user_password; ?>" class="form-control"
+                                name="user_password" id="">
+                        </div>
 
-<div class="form-group">
-    <input class="btn btn-primary" type="submit" name="edit_user" value="Update Profile">
-</div>
-</form>
+                        <div class="form-group">
+                            <input class="btn btn-primary" type="submit" name="edit_user" value="Update Profile">
+                        </div>
+                    </form>
 
 
 
